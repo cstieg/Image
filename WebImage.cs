11a86af13b14cd76162f0bf3060f08757d618fdd
cstieg <cstieg4899@yahoo.com>
2017-10-26 11:09:00 -0400

    Change: Image class to WebImage

---
+++ WebImage.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cstieg.Image
+{
+    public class WebImageBase
+    {
+        [Key]
+        public virtual int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public virtual string ImageUrl { get; set; }
+
+        [StringLength(1000)]
+        public virtual string ImageSrcSet { get; set; }
+
+        [StringLength(100)]
+        public virtual string Caption { get; set; }
+    }
+}
